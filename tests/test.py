e41b8fcf8a4b2762ecf86585e8758d75d1ab5c20
Ben Payton <bennyp2494@gmail.com>
2024-09-27 11:23:21 -0600

    I just want it to work

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,8 @@
 #Make Python Tests
 import pytest
-import ben_future_value as bfv
+import ben_future_value.future_value as bfv
 
-def test_future_value():
+def test_Future_Value():
     future_value = bfv.Future_Value(1000,7,200,40,4)
 
     assert future_value.Principle_value == 1000
