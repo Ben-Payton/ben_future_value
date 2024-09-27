4013cacbded293d9b5c7830cc04c8c7bd1c55f1b
Ben Payton <bennyp2494@gmail.com>
2024-09-27 10:50:10 -0600

    created first test

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,8 @@
 #Make Python Tests
+import pytest
+import ben_future_value as bfv
+
+def test_future_value():
+    future_value = bfv.Future_Value(1000,7,200,40,4)
+
+    assert future_value.Principle_value == 1000
